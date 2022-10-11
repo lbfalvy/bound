6204743d741f180c9dfc1b8d8b0fed2ecc7d10db
Lawrence Bethlenfalvy <lbfalvy@protonmail.com>
2022-10-11 22:02:36 +0100

    Fixed replacing guard with illegal value

--- src/bound.rs
+++ src/bound.rs
@@ -1,11 +1,9 @@
-use std::fmt::Display;
+use std::fmt::{Debug, Display};
 use std::future::IntoFuture;
-use std::ops::Deref;
-use std::ops::DerefMut;
-use std::mem;
-use std::ptr;
-use std::fmt::Debug;
 use std::hash::Hash;
+use std::mem;
+use std::ops::{Deref, DerefMut};
+use std::ptr;
 
 
 /// Derive a T from a U and use it safely. U is placed on the heap, then
@@ -40,19 +38,19 @@ use std::hash::Hash;
 /// 
 /// Normally you wouldn't be able to pass around a `RwLockWriteGuard`, but because this
 /// is just a struct with no lifetime parameters, this works intuitively with a Bound.
-pub struct Bound<T, U> {
+pub struct Bound<G, U> {
     // Needed so that Drop can consume the T.
     // Safety: this will always be Some() until Drop is called
-    derived: Option<T>,
+    derived: Option<G>,
     source_ptr: *mut U,
 }
 
-impl<'a, T, U: 'a> Bound<T, U> {
+impl<'a, G, L: 'a> Bound<G, L> {
     /// Bind data to a referrent
-    pub fn new<F: FnOnce(&'a mut U) -> T>(source: U, func: F) -> Self {
+    pub fn new<F: FnOnce(&'a mut L) -> G>(source: L, func: F) -> Self {
         // We need to create the box to avoid strange allocators
         let source_ref = Box::leak(Box::new(source));
-        let source_ptr = source_ref as *mut U;
+        let source_ptr = source_ref as *mut L;
         let derived = func(source_ref);
         Self {
             derived: Some(derived),
@@ -61,20 +59,20 @@ impl<'a, T, U: 'a> Bound<T, U> {
     }
 
     /// Bind data to a referrent or a referrent error
-    pub fn try_new<E, F>(source: U, func: F) -> Result<Self, Bound<E, U>>
-    where F: FnOnce(&'a mut U) -> Result<T, E>{
+    pub fn try_new<E, F>(source: L, func: F) -> Result<Self, Bound<E, L>>
+    where F: FnOnce(&'a mut L) -> Result<G, E>{
         let source_ref = Box::leak(Box::new(source));
-        let source_ptr = source_ref as *mut U;
+        let source_ptr = source_ref as *mut L;
         func(source_ref)
             .map(|res| Bound{ derived: Some(res), source_ptr })
             .map_err(|e| Bound{ derived: Some(e), source_ptr })
     }
 
     /// Bind data to an asynchronously obtained referrent
-    pub async fn async_new<Fut, F>(source: U, func: F) -> Self
-    where Fut: IntoFuture<Output = T>, F: FnOnce(&'a mut U) -> Fut {
+    pub async fn async_new<Fut, F>(source: L, func: F) -> Self
+    where Fut: IntoFuture<Output = G>, F: FnOnce(&'a mut L) -> Fut {
         let source_ref = Box::leak(Box::new(source));
-        let source_ptr = source_ref as *mut U;
+        let source_ptr = source_ref as *mut L;
         let derived = func(source_ref).await;
         Self {
             derived: Some(derived),
@@ -83,17 +81,17 @@ impl<'a, T, U: 'a> Bound<T, U> {
     }
 
     /// Bind data to an asynchronously obtained referrent or referrent error
-    pub async fn async_try_new<E, Fut, F>(source: U, func: F) -> Result<Self, Bound<E, U>>
-    where Fut: IntoFuture<Output = Result<T, E>>, F: FnOnce(&'a mut U) -> Fut {
+    pub async fn async_try_new<E, Fut, F>(source: L, func: F) -> Result<Self, Bound<E, L>>
+    where Fut: IntoFuture<Output = Result<G, E>>, F: FnOnce(&'a mut L) -> Fut {
         let source_ref = Box::leak(Box::new(source));
-        let source_ptr = source_ref as *mut U;
+        let source_ptr = source_ref as *mut L;
         func(source_ref).await
             .map(|res| Bound{ derived: Some(res), source_ptr })
             .map_err(|e| Bound{ derived: Some(e), source_ptr })
     }
 
     /// Drop T and get back U
-    pub fn unbind(mut self) -> Box<U> {
+    pub fn unbind(mut self) -> Box<L> {
         mem::drop(self.derived.take());
         // Safety: a mutable reference to source was held by derived which was dropped just above
         // in addition, this function consumes self so it will only run once, and this is the
@@ -102,14 +100,28 @@ impl<'a, T, U: 'a> Bound<T, U> {
         self.source_ptr = ptr::null_mut(); // Prevent Drop from deallocating source
         source
     }
+
+    /// Access the wrapped struct
+    pub fn wrapped(&self) -> &G {
+        // Safety: this is always a Some() until Drop is called
+        unsafe { self.derived.as_ref().unwrap_unchecked() }
+    }
+
+    /// Modify the wrapped struct. Making this public would be unsound because it might be
+    /// overwritten
+    fn wrapped_mut(&mut self) -> &mut G {
+        // Safety: this is always a Some() until Drop is called
+        unsafe { self.derived.as_mut().unwrap_unchecked() }
+    }
 }
 
 /// Ensure that
 /// - `derived` gets dropped before `source_ptr`
 /// - `source_ptr` gets dropped
-impl<T, U> Drop for Bound<T, U> {
+impl<G, L> Drop for Bound<G, L> {
     fn drop(&mut self) {
         mem::drop(self.derived.take());
+        // Self::unbind might take the U out of the struct and set the pointer to null
         if !self.source_ptr.is_null() {
             // Safety: A mutable reference to source was held by derived, but derived was dropped
             // just above therefore the Box may safely be dropped.
@@ -119,83 +131,98 @@ impl<T, U> Drop for Bound<T, U> {
     }
 }
 
-impl<T, U> Deref for Bound<T, U> {
+impl<T: ?Sized, G, U> Deref for Bound<G, U> where G: Deref<Target = T> {
     type Target = T;
     fn deref(&self) -> &Self::Target {
-        unsafe {
-            self.derived.as_ref().unwrap_unchecked()
-        }
+        self.wrapped().deref()
     }
 }
 
-impl<T, U> DerefMut for Bound<T, U> {
+impl<T: ?Sized, G, U> DerefMut for Bound<G, U> where G: DerefMut<Target = T> {
     fn deref_mut(&mut self) -> &mut Self::Target {
-        // Safety: this is always a Some() until Drop is called
-        unsafe {
-            self.derived.as_mut().unwrap_unchecked()
-        }
+        self.wrapped_mut().deref_mut()
     }
+}
+
+impl<T: ?Sized, G, L> AsRef<T> for Bound<G, L> where G: AsRef<T> {
+    fn as_ref(&self) -> &T { self.wrapped().as_ref() }
+}
+
+impl<T: ?Sized, G, L> AsMut<T> for Bound<G, L> where G: DerefMut + AsMut<T> {
+    fn as_mut(&mut self) -> &mut T { self.wrapped_mut().as_mut() }
 }
 
 #[cfg(test)]
 mod tests {
-    use std::sync::{Arc, RwLock};
+    use std::ops::Deref;
+    use std::sync::{Arc, RwLock, RwLockWriteGuard};
 
     use super::*;
 
-    struct Mock<'a>(&'a str, Option<&'a Mock<'a>>);
-    impl<'a> Drop for Mock<'a> {
+    struct MockStore(&'static str);
+    impl Drop for MockStore {
         fn drop(&mut self) {
-            let Mock(name, _) = self;
-            println!("{name} dropped");
+            println!("Store {} dropped", self.0);
             self.0 = "FAIL";
         }
     }
+    struct MockLock<'a>(&'a MockStore);
+    impl Drop for MockLock<'_> {
+        fn drop(&mut self) { println!("Lock for {} dropped", self.0.0) }
+    }
+    impl Deref for MockLock<'_> {
+        type Target = str;
+        fn deref(&self) -> &Self::Target {
+            self.0.0
+        }
+    }
 
-    fn mk_source() -> Mock<'static> { Mock("source", None) }
+    fn mk_store() -> MockStore { MockStore("flag") }
 
-    // /// This is a compile-time error (but we can't test that)
-    // fn mk_derived_naiive() -> Mock { Mock("derived", Some(&mk_source())) }
+    /// This is a compile-time error (but we can't test that)
+    // fn mk_lock_naiive() -> MockLock<'static> { MockLock(&mk_store()) }
 
     /// Basic usage of Bound as a smart pointer
-    fn mk_derived() -> Bound<Mock<'static>, Mock<'static>> {
-        Bound::new(mk_source(), |src| Mock("derived", Some(src)))
+    fn mk_lock() -> Bound<MockLock<'static>, MockStore> {
+        Bound::new(mk_store(), |s| MockLock(s))
     }
 
     #[test]
     fn test_functionality() {
-        let derived = mk_derived();
-        assert_eq!(derived.1.unwrap().0, "source");
-        assert_eq!(derived.0, "derived");
+        println!("-- testing basic functionality --");
+        let bound = mk_lock();
+        assert_eq!(bound.wrapped().0.0, "flag"); // can read wrapped (needed for error wrapping)
+        assert_eq!(bound.deref(), "flag"); // can deref through wrapped if Deref
     }
 
     #[test]
     fn test_unbind() {
-        let derived = mk_derived();
-        let source = derived.unbind();
+        println!("-- testing unbind --");
+        let source = {
+            let bound = mk_lock();
+            bound.unbind()
+        };
         println!("derived should be dropped but source is still alive");
-        assert_eq!(source.0, "source");
+        assert_eq!(source.0, "flag"); // source wasn't finalized with bound
+    }
+
+    /// This is extracted to demonstrate that the lock guard can have a static lifetime
+    fn get_writer() -> Bound<RwLockWriteGuard<'static, usize>, Arc<RwLock<usize>>> {
+        let shared_data = Arc::new(RwLock::new(1));
+        Bound::new(shared_data.clone(), |a| a.write().unwrap())
     }
 
     #[test]
     fn test_rwlock() {
-        let shared_data = Arc::new(RwLock::new(1));
-        let mut writer = Bound::new(shared_data.clone(), |a| a.write().unwrap());
-        **writer = 2;
+        println!("-- testing returning RwLockWriteGuard referencing Arc<RwLock> --");
+        let mut writer = get_writer();
+        *writer = 2;
     }
 }
 
 // Delegate std implementations to `self.deref()`
 
-impl<T, U, V> AsRef<V> for Bound<T, U> where T: AsRef<V> {
-    fn as_ref(&self) -> &V { self.deref().as_ref() }
-}
-
-impl<T, U, V> AsMut<V> for Bound<T, U> where T: AsMut<V> {
-    fn as_mut(&mut self) -> &mut V { self.deref_mut().as_mut() }
-}
-
-impl<T, U> Debug for Bound<T, U> where T: Debug {
+impl<G, L> Debug for Bound<G, L> where G: Debug {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         f.debug_tuple("Bound")
             .field(self.deref())
@@ -203,28 +230,30 @@ impl<T, U> Debug for Bound<T, U> where T: Debug {
     }
 }
 
-impl<T, U> Display for Bound<T, U> where T: Display {
+impl<T: ?Sized, G, L> Display for Bound<G, L> where G: Deref<Target= T>, T: Display {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         self.deref().fmt(f)
     }
 }
 
-impl<T, U> Hash for Bound<T, U> where T: Hash {
+impl<T: ?Sized, G, L> Hash for Bound<G, L> where G: Deref<Target = T>, T: Hash {
     fn hash<H: std::hash::Hasher>(&self, state: &mut H) { self.deref().hash(state) }
 }
 
-impl<T, U> Eq for Bound<T, U> where T: Eq {}
-impl<T, U, V, W> PartialEq<Bound<V, W>> for Bound<T, U> where T: PartialEq<V> {
-    fn eq(&self, other: &Bound<V, W>) -> bool { self.deref().eq(other.deref()) }
-    fn ne(&self, other: &Bound<V, W>) -> bool { self.deref().ne(other.deref()) }
+impl<T: ?Sized, G, L> Eq for Bound<G, L> where G: Deref<Target = T>, T: Eq {}
+impl<T: ?Sized, G, L, T2: ?Sized, G2, L2> PartialEq<Bound<G2, L2>> for Bound<G, L>
+where G: Deref<Target = T>, G2: Deref<Target = T2>, T: PartialEq<T2> {
+    fn eq(&self, other: &Bound<G2, L2>) -> bool { self.deref().eq(other.deref()) }
+    fn ne(&self, other: &Bound<G2, L2>) -> bool { self.deref().ne(other.deref()) }
 }
 
-impl<T, U, V, W> PartialOrd<Bound<V, W>> for Bound<T, U> where T: PartialOrd<V> {
-    fn partial_cmp(&self, other: &Bound<V, W>) -> Option<std::cmp::Ordering> {
+impl<T: ?Sized, G, L, T2: ?Sized, G2, L2> PartialOrd<Bound<G2, L2>> for Bound<G, L>
+where G: Deref<Target = T>, G2: Deref<Target = T2>, T: PartialOrd<T2> {
+    fn partial_cmp(&self, other: &Bound<G2, L2>) -> Option<std::cmp::Ordering> {
         self.deref().partial_cmp(&other.deref())
     }
 }
 
-impl<T, U> Ord for Bound<T, U> where T: Ord {
+impl<T: ?Sized, G, L> Ord for Bound<G, L> where G: Deref<Target = T>, T: Ord {
     fn cmp(&self, other: &Self) -> std::cmp::Ordering { self.deref().cmp(other.deref()) }
 }
